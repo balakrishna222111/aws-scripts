75c072eaada8a277c2a23ef6e2f94b602957ffeb
Balakrishna Teeda <balakrishna.t@pogeyan.com>
2020-02-06 21:51:13 +0530

    Provisioning instances and run userdata scripts

--- backup-to-s3-bucket.sh
+++ backup-to-s3-bucket.sh
@@ -1,8 +1,11 @@
 backup_bucket_name="" ## Add the s3 bucket name 
 backupfile_location=""  ## Add the backup location
+latestBackupFile=$(ls -t | head -1)
 
 upload_s3(){
-    aws s3 cp $backupfile_location s3://$backup_bucket_name --recursive
+    aws s3 cp $backupfile_location/$latestBackupFile s3://$backup_bucket_name --recursive
 }
 
 upload_s3
+
+echo $latestBackupFile
